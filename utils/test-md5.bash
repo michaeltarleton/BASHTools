9da3b6067b90a3bec093b32a9fde72026ad2dc61
Michael Tarleton <michael.tarleton@me.com>
2017-01-27 14:45:53 -0600

    master: Removed echo line and added some more desciption text

--- utils/test-md5.bash
+++ utils/test-md5.bash
@@ -3,10 +3,10 @@
 if [ -z $1 ] || [ -z $2 ]; then
     echo 'Usage:'
     echo "\t$0 filename MD5Hash"
+    echo "\tIf no output is shown then the hashes match"
     exit 1
 fi
 
 PATH="$PATH:/sbin"
 
-echo "diff <(md5 $1 | awk '{print \$4}') <(echo $2)"
 diff <(md5 $1 | awk '{print $4}') <(echo $2)
